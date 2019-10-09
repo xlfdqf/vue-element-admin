06e01ab20130e6ea21422c4c95b8ef1c6bc6c5ba
xulanfang <1574425015@qq.com>
2019-10-09 17:08:25 +0800

    update permission

--- mock/role/routes.js
+++ mock/role/routes.js
@@ -49,35 +49,32 @@ export const constantRoutes = [
 export const asyncRoutes = [
   {
     path: '/permission',
-    component: 'layout/Layout',
-    redirect: '/permission/index',
-    alwaysShow: true,
+    component: 'layout',
+    redirect: '/permission/page',
+    name: 'Permission',
     meta: {
-      title: '权限页22',
+      title: '权限页11',
       icon: 'lock',
       roles: ['admin', 'editor']
     },
     children: [
       {
         path: 'page',
-        component: 'views/permission/page',
+        component: 'page',
         name: 'PagePermission',
         meta: {
-          title: '页面权限22',
-          roles: ['admin']
+          title: '页面权限11',
         }
-      },
-      {
+      }, {
         path: 'directive',
-        component: 'views/permission/directive',
+        component: 'directive',
         name: 'DirectivePermission',
         meta: {
           title: '指令权限'
         }
-      },
-      {
+      }, {
         path: 'role',
-        component: 'views/permission/role',
+        component: 'role',
         name: 'RolePermission',
         meta: {
           title: '角色权限',
@@ -85,176 +82,21 @@ export const asyncRoutes = [
         }
       }
     ]
-  },
-
-  {
+  }, {
     path: '/icon',
-    component: 'layout/Layout',
+    name: 'Icon',
+    component: 'layout',
+    redirect: '/icon/index',
     children: [
       {
         path: 'index',
-        component: 'views/icons/index',
+        component: 'icons',
         name: 'Icons',
-        meta: { title: '图标', icon: 'icon', noCache: true }
+        meta: {
+          title: '图标11',
+          icon: 'icon'
+        }
       }
     ]
-  },
-
-  {
-    path: '/components',
-    component: 'layout/Layout',
-    redirect: 'noRedirect',
-    name: 'ComponentDemo',
-    meta: {
-      title: '组件',
-      icon: 'component'
-    },
-    children: [
-      {
-        path: 'tinymce',
-        component: 'views/components-demo/tinymce',
-        name: 'TinymceDemo',
-        meta: { title: '富文本编辑器' }
-      },
-      {
-        path: 'avatar-upload',
-        component: 'views/components-demo/avatar-upload',
-        name: 'AvatarUploadDemo',
-        meta: { title: '图片上传' }
-      },
-      {
-        path: 'dropzone',
-        component: 'views/components-demo/dropzone',
-        name: 'DropzoneDemo',
-        meta: { title: 'Dropzone' }
-      },
-      {
-        path: 'back-to-top',
-        component: 'views/components-demo/back-to-top',
-        name: 'BackToTopDemo',
-        meta: { title: '返回顶部' }
-      },
-    ]
-  },
-  {
-    path: '/nested',
-    component: 'layout/Layout',
-    redirect: '/nested/menu1/menu1-1',
-    name: 'Nested',
-    meta: {
-      title: '路由嵌套',
-      icon: 'nested'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: 'views/nested/menu1/index',
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        redirect: '/nested/menu1/menu1-1',
-        children: [
-          {
-            path: 'menu1-1',
-            component: 'views/nested/menu1/menu1-1',
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: 'views/nested/menu1/menu1-2',
-            name: 'Menu1-2',
-            redirect: '/nested/menu1/menu1-2/menu1-2-1',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: 'views/nested/menu1/menu1-2/menu1-2-1',
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: 'views/nested/menu1/menu1-2/menu1-2-2',
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: 'views/nested/menu1/menu1-3',
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        name: 'Menu2',
-        component: 'views/nested/menu2/index',
-        meta: { title: 'Menu2' }
-      }
-    ]
-  },
-  {
-    path: '/error',
-    component: 'layout/Layout',
-    redirect: 'noRedirect',
-    name: 'ErrorPages',
-    meta: {
-      title: '错误页面',
-      icon: '404'
-    },
-    children: [
-      {
-        path: '401',
-        component: 'views/error-page/401',
-        name: 'Page401',
-        meta: { title: 'Page 401', noCache: true }
-      },
-      {
-        path: '404',
-        component: 'views/error-page/404',
-        name: 'Page404',
-        meta: { title: 'Page 404', noCache: true }
-      }
-    ]
-  },
-  {
-    path: '/excel',
-    component: 'layout/Layout',
-    redirect: '/excel/export-excel',
-    name: 'Excel',
-    meta: {
-      title: 'Excel',
-      icon: 'excel'
-    },
-    children: [
-      {
-        path: 'export-excel',
-        component: 'views/excel/export-excel',
-        name: 'ExportExcel',
-        meta: { title: '导出 Excel' }
-      },
-      {
-        path: 'export-selected-excel',
-        component: 'views/excel/select-excel',
-        name: 'SelectExcel',
-        meta: { title: '导出已选择项' }
-      },
-      {
-        path: 'export-merge-header',
-        component: 'views/excel/merge-header',
-        name: 'MergeHeader',
-        meta: { title: '导出多级表头' }
-      },
-      {
-        path: 'upload-excel',
-        component: 'views/excel/upload-excel',
-        name: 'UploadExcel',
-        meta: { title: '上传excel' }
-      }
-    ]
-  },
-  { path: '*', redirect: '/404', hidden: true }
+  }
 ]
--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,62 +1,61 @@
 // import { asyncRoutes, constantRoutes } from '@/router'
-// import { asyncRoutes } from '../../../mock/role/routes'
+import { asyncRoutes as serverRouterMap } from '../../../mock/role/routes'
 import { constantRoutes, routerMap } from '@/router'
-// import Layout from '@/layout/index'
 //假设为后端返回数据
-const serverRouterMap = [
-  {
-    path: '/permission',
-    component: 'layout',
-    redirect: '/permission/page',
-    name: 'Permission',
-    meta: {
-      title: '权限页11',
-      icon: 'lock',
-      roles: ['admin', 'editor']
-    },
-    children: [
-      {
-        path: 'page',
-        component: 'page',
-        name: 'PagePermission',
-        meta: {
-          title: '页面权限11',
-        }
-      }, {
-        path: 'directive',
-        component: 'directive',
-        name: 'DirectivePermission',
-        meta: {
-          title: '指令权限'
-        }
-      }, {
-        path: 'role',
-        component: 'role',
-        name: 'RolePermission',
-        meta: {
-          title: '角色权限',
-          roles: ['admin']
-        }
-      }
-    ]
-  }, {
-    path: '/icon',
-    name: 'Icon',
-    component: 'layout',
-    redirect: '/icon/index',
-    children: [
-      {
-        path: 'index',
-        component: 'icons',
-        name: 'Icons',
-        meta: {
-          title: '图标11',
-          icon: 'icon'
-        }
-      }
-    ]
-  }
-]
+// const serverRouterMap = [
+//   {
+//     path: '/permission',
+//     component: 'layout',
+//     redirect: '/permission/page',
+//     name: 'Permission',
+//     meta: {
+//       title: '权限页11',
+//       icon: 'lock',
+//       roles: ['admin', 'editor']
+//     },
+//     children: [
+//       {
+//         path: 'page',
+//         component: 'page',
+//         name: 'PagePermission',
+//         meta: {
+//           title: '页面权限11',
+//         }
+//       }, {
+//         path: 'directive',
+//         component: 'directive',
+//         name: 'DirectivePermission',
+//         meta: {
+//           title: '指令权限'
+//         }
+//       }, {
+//         path: 'role',
+//         component: 'role',
+//         name: 'RolePermission',
+//         meta: {
+//           title: '角色权限',
+//           roles: ['admin']
+//         }
+//       }
+//     ]
+//   }, {
+//     path: '/icon',
+//     name: 'Icon',
+//     component: 'layout',
+//     redirect: '/icon/index',
+//     children: [
+//       {
+//         path: 'index',
+//         component: 'icons',
+//         name: 'Icons',
+//         meta: {
+//           title: '图标11',
+//           icon: 'icon'
+//         }
+//       }
+//     ]
+//   }
+// ]
 // 将本地routerMap映射到ajax获取到的serverRouterMap;
 function generateAsyncRouter(routerMap, serverRouterMap) {
   serverRouterMap.forEach(function (item, index) {
